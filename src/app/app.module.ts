d09d2dd710410e5df186055b5bb9cd6ca2cccc2c
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2022-10-07 16:14:06 +0400

    Build: error handling and repeat functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import localeRu from '@angular/common/locales/ru';
+import { registerLocaleData } from '@angular/common';
+
 import { AppComponent } from './app.component';
 import { RateComponent } from './components/rate/rate.component';
+
+
+registerLocaleData(localeRu, 'ru-RU');
 
 
 @NgModule({
@@ -15,7 +21,9 @@ import { RateComponent } from './components/rate/rate.component';
         AppComponent,
         RateComponent
     ],
-    providers: [],
+    providers: [
+        { provide: LOCALE_ID, useValue: 'ru-RU' }
+    ],
     bootstrap: [
         AppComponent
     ]
